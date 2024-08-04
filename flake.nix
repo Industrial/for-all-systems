2773b49bad8988683d14340b2aafd687318fa797
Tom Wieland <tom.wieland@gmail.com>
2024-08-04 11:57:04 +0000

    refactor(flake): expose only the function

--- flake.nix
+++ flake.nix
@@ -5,18 +5,5 @@
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
   };
-  outputs = inputs @ {self, ...}: {
-    lib = {
-      default = import ./lib/forAllSystems.nix;
-    };
-    # Eat your own dog food.
-    devShells = import ./lib/forAllSystems.nix inputs.nixpkgs inputs.nixpkgs.lib.systems.flakeExposed ({
-      system,
-      pkgs,
-    }: {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [hello];
-      };
-    });
-  };
+  outputs = inputs @ {self, ...}: import ./lib/forAllSystems.nix;
 }
