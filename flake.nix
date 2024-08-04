8943455c98d59c84dcb9353ab6c40134840e3b84
Tom Wieland <tom.wieland@gmail.com>
2024-08-04 11:50:11 +0000

    feat: eat your own dogfood

--- flake.nix
+++ flake.nix
@@ -3,11 +3,20 @@
   inputs = {
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
-    }
+    };
   };
   outputs = inputs @ {self, ...}: {
     lib = {
       default = import ./lib/forAllSystems.nix;
     };
+    # Eat your own dog food.
+    devShells = import ./lib/forAllSystems.nix inputs.nixpkgs inputs.nixpkgs.lib.systems.flakeExposed ({
+      system,
+      pkgs,
+    }: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [hello];
+      };
+    });
   };
 }
