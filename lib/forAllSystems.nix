cfc2e009176bcda2b556bbe4dd421d44c7944ba1
Tom Wieland <tom.wieland@gmail.com>
2024-08-04 12:13:44 +0000

    refactor(forAllSystems): pass options with defaults

--- lib/forAllSystems.nix
+++ lib/forAllSystems.nix
@@ -1,9 +1,21 @@
-nixpkgs: systems: f:
-nixpkgs.lib.genAttrs systems (system:
+{
+  # nixpkgs: The nixpkgs to use, defaulting to importing <nixpkgs> from Nix
+  #          search path.
+  nixpkgs ? import <nixpkgs> {},
+  # systems: A list of systems to be used. Default value is taken from
+  #          nixpkgs.lib.systems.flakeExposed.
+  systems ? nixpkgs.lib.systems.flakeExposed,
+  # config: Configuration for importing nixpkgs, allowing unfree packages by
+  #         default.
+  config ? {allowUnfree = true;},
+}: f: let
+  pkgsForSystem = system:
+    import nixpkgs {
+      inherit system config;
+    };
+in
+  nixpkgs.lib.genAttrs systems (system:
     f {
       inherit system;
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {allowUnfree = true;};
-      };
+      pkgs = pkgsForSystem system;
     })
